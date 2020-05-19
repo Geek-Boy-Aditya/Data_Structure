e058f9e7795479a37997dff6b8cdc40540de0175
Aditya Raj <65248775+Geek-Boy-Aditya@users.noreply.github.com>
2020-05-19 22:14:40 +0530

    Add files via upload

---
+++ Character_Array/Remove_Duplicate_Frrom_String.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+// remov
+void remove(char a[])
+{
+	int l=strlen(a);
+	if(l==1 or l==0)
+	{
+		return;
+	}
+	int prev=0;
+	for (int current =1;current<l;current++)
+	{
+		if (a[current] !=a[prev])
+		{
+		
+		prev++;
+		a[prev]=a[current];
+    	}
+	}
+	a[prev+1]='\0';
+	return;
+}
+
+int main()
+{
+	char a [100];
+	cin.getline(a,100);
+	remove(a);
+	
+	cout<<a;
+	//return 0;
+}
